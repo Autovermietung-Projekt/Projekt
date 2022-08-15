8670bb12400caf99cb87c3cac106173066d833d1
Autovermietung-Projekt <111274143+Autovermietung-Projekt@users.noreply.github.com>
2022-08-15 12:20:01 +0200

    Add files via upload

---
+++ _db.php
@@ -0,0 +1,43 @@
+<?php
+  // This defines the connection data to connect to the database
+  $config = [
+    'dbserver' => 'sql.local',
+    'dbuser' => 'db349760023',
+    'dbpassword' => 'wy6ZNhp3',
+    'dbname' => 'db349760023'
+  ];
+
+  // This creates a connection to the database
+  function dbConnect() {
+    global $config;
+    $dsn = "mysql:host={$config['dbserver']};dbname={$config['dbname']};charset=utf8mb4";
+    $pdo = null;
+    try {
+      $pdo = new PDO($dsn, $config['dbuser'], $config['dbpassword']);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+      $pdo->setAttribute(PDO::MYSQL_ATTR_FOUND_ROWS, true);
+      return $pdo;
+    }
+    catch(PDOException $e)
+    {
+      echo ':-(';
+    }
+  }
+
+  function showTables($pdo) {
+    $sql = "SHOW TABLES";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    $num = $stmt->rowCount();
+    return [$num, $result];
+  }
+
+  // $pdo = dbConnect();
+  // $res = showTables($pdo);
+  // echo "<pre>";
+  // print_r($res);
+  // echo "</pre>";
+?>
